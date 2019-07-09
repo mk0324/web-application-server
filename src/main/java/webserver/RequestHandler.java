c097bf0c98b0af276bc4a4139338d948c6d9e72a
Mung <munkyung0324@gmail.com>
2019-07-09 14:38:49 +0900

    요구사항1 리팩토링
    - HttpRequestUtils에 url path 처리 함수 생성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -30,16 +32,23 @@ public class RequestHandler extends Thread {
                 return;
             }
 
+            /*User user = new User();
             String[] tokens = line.split(" ");
+            String[] uri = tokens[1].split("\\?");
+            String[] userInfo = uri[1].split("&");
+            for(String s : userInfo){
+                //user= s.split("=")[1];
+            }*/
 
-            while (!line.equals("")){
+            /*while (!line.equals("")){
                 line = br.readLine();
                 log.debug("header : {}", line);
-            }
+            }*/
 
+            String url = HttpRequestUtils.getUrl(line);
             DataOutputStream dos = new DataOutputStream(out);
             //byte[] body = "Hello World".getBytes();
-            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
