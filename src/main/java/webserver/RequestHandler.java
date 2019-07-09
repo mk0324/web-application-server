fae1ce7ac1edded794e8f59b65f21c8d76f47614
Mung <munkyung0324@gmail.com>
2019-07-09 22:23:24 +0900

    요구사항3 정리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import model.User;
@@ -28,50 +29,48 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-            String firstLine = br.readLine();
-            String line = firstLine;
-            int contentLenth = -1;
-            //log.debug("request line : {}", line);
-
-            if(line == null){
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            //int contentLenth = -1;
+            if (line == null) {
                 return;
             }
 
-            while (!line.equals("")){
+            String url = HttpRequestUtils.getUrl(line);
+            log.debug("request line : {}", line);
+            br.readLine();
+
+            Map<String, String> headers = new HashMap<>();
+            while (!line.equals("")) {
                 log.debug("header : {}", line);
-                if(line.startsWith("Content-Length")){
-                    contentLenth = parseInt(line.split(" ")[1]);
+                String[] headerTokens = line.split(": ");
+                if (headerTokens.length == 2) {
+                    headers.put(headerTokens[0], headerTokens[1]);
                 }
+                /*if(line.startsWith("Content-Length")){
+                    contentLenth = parseInt(line.split(" ")[1]);
+                }*/
                 line = br.readLine();
             }
+            log.debug("Content-Length : {}", headers.get("Content-Length"));
 
-            String url = HttpRequestUtils.getUrl(firstLine);
-
-            if(url.startsWith("/user/create")) {
+            if (url.equals("/user/create")) {
                 //int index = url.indexOf("?");
                 //String paramStr = url.substring(index + 1);
-                String paramStr = null;
-                if(contentLenth != -1) {
-                    paramStr = IOUtils.readData(br, contentLenth);
-                }
+                String paramStr = IOUtils.readData(br, parseInt(headers.get("Content-Length")));
+                log.debug("Request Body : {}", paramStr);
 
                 Map<String, String> params = HttpRequestUtils.parseQueryString(paramStr);
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 log.debug("User : {}", user);
 
-                DataOutputStream dos = new DataOutputStream(out);
-                byte[] body = "가입에 성공".getBytes();
-                //byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-                response200Header(dos, body.length);
-                responseBody(dos, body);
-            }else {
+                url = "/index.html";
+            }
                 DataOutputStream dos = new DataOutputStream(out);
                 //byte[] body = "Hello World".getBytes();
                 byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
                 response200Header(dos, body.length);
                 responseBody(dos, body);
-            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
